bbf5564b4751cac3ccabfdb78254d3cb7ce8e68a
chandresh12 <chandreshkulshrestha12@gmail.com>
2024-02-21 22:17:17 +0530

    first commit

---
+++ conditional.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  /*  int a;
+    cin>>a;
+    if(a>0){
+        cout<<"a is postive number"<<endl;
+    }
+    else{
+        cout<<"a is negative number"<<endl;
+    }
+    */
+
+   /*int a,b;
+   cin>>a>>b;
+   cout<<"value of a and b is "<<a<<" "<<b<<endl;
+   */
+ /*int a,b;
+ cout<<"enter the value of a"<<endl;
+ cin>>a;
+ cout<<"enter the value of b"<<endl;
+ cin>>b;
+
+ if (a>b){
+    cout<<"a is greater number"<<endl;
+ }
+  if (b>a){
+  cout<<"b is greater number"<<endl;
+  }
+  */
+ /* int a;
+  cout<<"enter the value of a"<<endl;
+  cin>>a;
+
+  if (a>0){
+    cout<<"number is postive"<<endl;
+  }    
+  else{
+    if (a<0){
+        cout<<"number is negative"<<endl;
+    }
+    else{
+        cout<<"a is 0"<< endl;
+    }
+  }*/
+  /*int a=9;
+  if(a==9){
+    cout<<"niney"<<endl;
+  }  
+  if(a>0){
+    cout<<"postive"<<endl;
+  }
+  else{
+    cout<<"negative"<<endl;
+  }*/
+ /*int a=2;
+ int b= a+1;
+ if((a=3)==b){
+    cout<<a;
+ } 
+ else{
+    cout<<a+1;
+
+ }*/
+int a=30;
+if (a>24){
+    cout<<"chandresh";
+
+}
+else{
+    if (a==24){
+        cout<<"ck";
+    }
+    else{
+        cout<<"kulshestha";
+    }
+}
+
+
+
+}
+ 
+
+
---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    /*
+    cout <<"nameste dunia" << endl;
+    
+
+unsigned int a = -112;
+cout << a << endl;
+*/
+int a= 5;
+int b=10;
+bool first = (a==b);
+cout<<first<<endl;
+
+bool second = (a>=b);
+cout<<second<<endl;
+ 
+bool third = (a<=b);
+cout<<third<<endl;
+
+
+}
