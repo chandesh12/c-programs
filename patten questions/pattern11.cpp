002a0c35db6d144b6dca212e75bd22f9022db9e4
chandresh12 <chandreshkulshrestha12@gmail.com>
2024-03-08 18:32:37 +0530

    pattern

---
+++ patten questions/pattern11.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=1;                                                 // NOT COMPLETE      ABCDEF//
+    while (i<=n)
+    {
+        int j=1;
+        char value=' A';
+        while (j<=i)
+
+        {
+            
+            cout<<'A';
+            value= 'A'+1;
+        j=j+1;
+            
+
+
+        }
+        cout<<endl;
+        i=i+1;
+
+    }
+    
+}
